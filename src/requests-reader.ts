a49092740d03181ba70160e82482718a8a97ac08
Przemo199 <przemo199@onet.pl>
2021-09-25 03:05:25 +0100

    Refactored requests reading logic and extracted it into a function

--- src/requests-reader.ts
+++ src/requests-reader.ts
@@ -1,15 +1,17 @@
 import fs from 'fs';
 import {RequestedGames} from './interfaces';
 
-const EXACT_MATCH_TAG = '[exact_match]';
-const ANY_MATCH_TAG = '[any_match]';
-const NO_MATCH_TAG = '[no_match]';
+enum Tag {
+    EXACT_MATCH = '[exact_match]',
+    ANY_MATCH = '[any_match]',
+    NO_MATCH = '[no_match]'
+}
 
-const REQUESTS_TXT_PATH = './requests.txt';
+const REQUESTS_FILE = './requests.txt';
 
 function exists(): void {
-    if (!fs.existsSync(REQUESTS_TXT_PATH)) {
-        fs.writeFileSync(REQUESTS_TXT_PATH, `${EXACT_MATCH_TAG}\n\n${ANY_MATCH_TAG}\n\n${NO_MATCH_TAG}\n`);
+    if (!fs.existsSync(REQUESTS_FILE)) {
+        fs.writeFileSync(REQUESTS_FILE, `${Tag.EXACT_MATCH}\n\n${Tag.ANY_MATCH}\n\n${Tag.NO_MATCH}\n`);
         throw new Error('requests.txt not found, its scaffold has been created in root directory');
     }
 }
@@ -18,66 +20,51 @@ function sortEntries(): void {
     console.time('Sorting and filtering entries');
     const games = readGames();
     const linesToWrite = [
-        EXACT_MATCH_TAG,
+        Tag.EXACT_MATCH,
         ...Array.from(new Set(games.exactMatches)).sort(),
-        ANY_MATCH_TAG,
+        Tag.ANY_MATCH,
         ...Array.from(new Set(games.anyMatches)).sort(),
-        NO_MATCH_TAG,
+        Tag.NO_MATCH,
         ...Array.from(new Set(games.noMatches)).sort()
-    ];
-    fs.writeFileSync(REQUESTS_TXT_PATH, linesToWrite.join('\n') + '\n');
+    ].join('\n') + '\n';
+    fs.writeFileSync(REQUESTS_FILE, linesToWrite);
     console.timeEnd('Sorting and filtering entries');
 }
 
 function readLines(): string[] {
-    return fs.readFileSync(REQUESTS_TXT_PATH).toString().toLowerCase()
-    .split('\n').map(line => line.trim()).filter(line => line !== '');
+    return fs.readFileSync(REQUESTS_FILE).toString().toLowerCase()
+        .split('\n').map(line => line.trim()).filter(line => line !== '');
+}
+
+function extractTitlesByTag(tag: Tag, lines: string[]): string[] {
+    const tagIndex =  lines.indexOf(tag);
+
+    const tagMatches = [];
+    if (tagIndex > -1) {
+        for (let i = tagIndex + 1; i < lines.length; i++) {
+            if (Object.values<string>(Tag).includes(lines[i])) {
+                break;
+            } else {
+                tagMatches.push(lines[i]);
+            }
+        }
+    }
+
+    return tagMatches;
 }
 
 function readGames(): RequestedGames {
     const lines = readLines();
-    const exactMatchIndex = lines.indexOf(EXACT_MATCH_TAG);
-    const anyMatchIndex = lines.indexOf(ANY_MATCH_TAG);
-    const noMatchIndex = lines.indexOf(NO_MATCH_TAG);
-    const exactMatches: string[] = [];
-    const anyMatches: string[] = [];
-    const noMatches: string[] = [];
 
-    if (exactMatchIndex > -1) {
-        for (let i = exactMatchIndex + 1; i < lines.length; i++) {
-            if ([ANY_MATCH_TAG, NO_MATCH_TAG].indexOf(lines[i]) > -1) {
-                break;
-            } else {
-                exactMatches.push(lines[i]);
-            }
-        }
+    if (lines.indexOf(Tag.EXACT_MATCH) === -1 && lines.indexOf(Tag.ANY_MATCH) === -1) {
+        throw new Error('No required tag found in requests.txt');
     }
 
-    if (anyMatchIndex > -1) {
-        for (let i = anyMatchIndex + 1; i < lines.length; i++) {
-            if ([EXACT_MATCH_TAG, NO_MATCH_TAG].indexOf(lines[i]) > -1) {
-                break;
-            } else {
-                anyMatches.push(lines[i]);
-            }
-        }
-    }
-
-    if (noMatchIndex > -1) {
-        for (let i = noMatchIndex + 1; i < lines.length; i++) {
-            if ([EXACT_MATCH_TAG, ANY_MATCH_TAG].indexOf(lines[i]) > -1) {
-                break;
-            } else {
-                noMatches.push(lines[i]);
-            }
-        }
-    }
-
-    if (exactMatchIndex === -1 && anyMatchIndex === -1) {
-        throw new Error('no match tag found in requests.txt');
-    }
-
-    return {exactMatches, anyMatches, noMatches};
+    return {
+        exactMatches: extractTitlesByTag(Tag.EXACT_MATCH, lines),
+        anyMatches: extractTitlesByTag(Tag.ANY_MATCH, lines),
+        noMatches: extractTitlesByTag(Tag.NO_MATCH, lines)
+    };
 }
 
 function getRequestedGames(): RequestedGames {
